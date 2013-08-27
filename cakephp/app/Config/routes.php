f7eb2f896fc5d1cf085a9f020f277afb5b5d0ade
Naoto Mawatari <github.mawatari@gmail.com>
2013-08-27 10:40:57 +0900

    API: Routerの設定

--- cakephp/app/Config/routes.php
+++ cakephp/app/Config/routes.php
@@ -21,6 +21,10 @@
  * @since         CakePHP(tm) v 0.2.9
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
+
+Router::mapResources('todos');
+Router::parseExtensions('json');
+
 /**
  * Here, we are connecting '/' (base path) to controller called 'Pages',
  * its action called 'display', and we pass a param to select the view file
